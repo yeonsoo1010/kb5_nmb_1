7680162f4672230c4780f42b26229f2f7a713e28
Cho Yeonsoo <128021464+yeonsoo1010@users.noreply.github.com>
2024-06-11 16:41:24 +0900

    Delete SkeletonProject - 복사본 directory

--- SkeletonProject - 복사본/src/pages/EditItem.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <router-link class="btn btn-primary" to="/edititem/:id"> 할일 수정 </router-link>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style scoped></style>
--- SkeletonProject - 복사본/src/pages/Home.vue
+++
@@ -1,6 +0,0 @@
-<template></template>
-
-<script setup>
-import {computed} from vue;
-import {use_money_list_store} from "@/stores/ItemList.js";
-</script>
