3448d586d9e4f755a2d487135d854dff015b3d87
sooho <soolmagpie@naver.com>
2024-06-11 15:58:00 +0900

    [feat] header, profile 업데이트

---
+++ SkeletonProject/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-sm bg-warning navbar-dark sticky-top">
+    <div class="container justify-content-between">
+      <router-link to="/" class="navbar-brand">가계부</router-link>
+      <div class="collapse navbar-collapse" id="mynavbar1">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <PieChartLink />
+          </li>
+        </ul>
+      </div>
+      <div class="d-flex">
+        <div class="collapse navbar-collapse d-none d-sm-block" id="mynavbar">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <router-link to="/profile" class="nav-link">
+                <i class="fa-solid fa-user-tie"></i> 프로필
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+<script setup></script>
