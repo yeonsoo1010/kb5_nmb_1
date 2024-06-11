0e17134cdf1f7331628996393b531fd7ff44b0f8
Angela <wpfkf4644@gmail.com>
2024-06-11 20:09:46 +0900

    home:16_error_목록이 보이지 않음

--- SkeletonProject/src/App.vue
+++ SkeletonProject/src/App.vue
@@ -1,10 +1,17 @@
 <template>
-    <div class="container">
-        <Header> </Header>
-        <router-view></router-view>
-    </div>
+  <div class="container">
+    <Header> </Header>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup>
-import Header from "./components/Header.vue";
+import { computed } from 'vue';
+import Header from '@/components/Header.vue';
+import { use_money_list_store } from '@/stores/ItemList.js';
+
+const money_list_store = use_money_list_store();
+const fetch_money_list = money_list_store.fetch_money_list;
+
+fetch_money_list();
 </script>
--- SkeletonProject/src/pages/HomeItem.vue
+++ SkeletonProject/src/pages/HomeItem.vue
@@ -1,19 +1,39 @@
 <template>
-    <li :class="money.done ? 'list-group-item list-group-item-success' : 'list-group-item'">
-        <span class="float-end badge bg-secondary pointer m-1" @click="router.push(`/edititem/${money.id}`)"> 편집</span>
-        <span class="float-end badge bg-secondary pointer m-1" @click="delete_money(money.id)"> 삭제</span>
-    </li>
+  <li
+    :class="
+      moneyItem.done
+        ? 'list-group-item list-group-item-success'
+        : 'list-group-item'
+    "
+  >
+    <span
+      class="float-end badge bg-secondary pointer m-1"
+      @click="router.push(`/items/edit/:id/${moneyItem.id}`)"
+    >
+      편집</span
+    >
+    <span
+      class="float-end badge bg-secondary pointer m-1"
+      @click="deleteMoneyItem(moneyItem.id)"
+    >
+      삭제</span
+    >
+  </li>
 </template>
 
 <script setup>
-import { useRouter } from "vue-router";
-import { use_money_list_store } from "@/stores/ItemList.js";
+import { useRouter } from 'vue-router';
+import { use_money_list_store } from '@/stores/ItemList.js';
 
 defineProps({
-    money: { Type: Object, required: true },
+  moneyItem: { type: Object, required: true },
 });
 
 const router = useRouter();
 const money_list_store = use_money_list_store();
 const { delete_money } = money_list_store;
+
+const deleteMoneyItem = (id) => {
+  delete_money(id);
+};
 </script>
