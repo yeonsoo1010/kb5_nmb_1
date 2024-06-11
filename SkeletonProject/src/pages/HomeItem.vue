40504bf946f498f2400c5ef44856e702a79a8a52
yeonsoo1010 <meriel1010@hanyang.ac.kr>
2024-06-11 17:56:16 +0900

    [feat] homeitem 수정

--- SkeletonProject/src/pages/HomeItem.vue
+++ SkeletonProject/src/pages/HomeItem.vue
@@ -1,16 +1,19 @@
 <template>
-    <div></div>
+    <li :class="money.done ? 'list-group-item list-group-item-success' : 'list-group-item'">
+        <span class="float-end badge bg-secondary pointer m-1" @click="router.push(`/edititem/${money.id}`)"> 편집</span>
+        <span class="float-end badge bg-secondary pointer m-1" @click="delete_money(money.id)"> 삭제</span>
+    </li>
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
-import { useTodoListStore } from "@/stores/index.js";
+import { use_money_list_store } from "@/stores/ItemList.js";
 
 defineProps({
-    todoItem: { Type: Object, required: true },
+    money: { Type: Object, required: true },
 });
 
 const router = useRouter();
 const money_list_store = use_money_list_store();
-const { deleteTodo } = todoListStore;
+const { delete_money } = money_list_store;
 </script>
--- SkeletonProject/src/stores/ItemList.js
+++ SkeletonProject/src/stores/ItemList.js
@@ -22,15 +22,77 @@ export const use_money_list_store = defineStore("money_list", () => {
         }
     };
 
-    // money_list 아이템 추가
-    // money_list 아이템 수정(변경)
-    // money_list 삭제
+    // 새로운 money_list 아이템 추가합니다. -- +, - 버튼
+    const add_money = async ({ todo, desc }, successCallback) => {
+        try {
+            const payload = { todo, desc };
+            const response = await axios.post(BASEURI, payload);
+            if (response.status === 201) {
+                state.todoList.push({ ...response.data, done: false });
+                successCallback();
+            } else {
+                alert("Todo 추가 실패");
+            }
+        } catch (error) {
+            alert("에러발생 :" + error);
+        }
+    };
+
+    const minus_money = async ({ todo, desc }, successCallback) => {
+        try {
+            const payload = { todo, desc };
+            const response = await axios.post(BASEURI, payload);
+            if (response.status === 201) {
+                state.todoList.push({ ...response.data, done: false });
+                successCallback();
+            } else {
+                alert("Todo 추가 실패");
+            }
+        } catch (error) {
+            alert("에러발생 :" + error);
+        }
+    };
+
+    // 기존 money_list 아이템 변경합니다.
+    const update_money = async ({ id, name, category_id, price, date, memo }, successCallback) => {
+        try {
+            const payload = { id, name, category_id, price, date, memo };
+            const response = await axios.put(BASEURI + `/${id}`, payload);
+            if (response.status === 200) {
+                let index = state.money_list.findIndex((money) => money.id === id);
+                state.money_list[index] = payload;
+                successCallback();
+            } else {
+                alert("기록 변경 실패");
+            }
+        } catch (error) {
+            alert("에러발생 :" + error);
+        }
+    };
+
+    //기존  money_list 아이템 삭제합니다.
+    const delete_money = async (id) => {
+        try {
+            const response = await axios.delete(BASEURI + `/${id}`);
+            if (response.status === 200) {
+                let index = state.money_list.findIndex((money) => money.id === id);
+                state.money_list.splice(index, 1);
+            } else {
+                alert("기록 삭제 실패");
+            }
+        } catch (error) {
+            alert("에러발생 :" + error);
+        }
+    };
 
     // money_list 반응성 주기
     const money_list = computed(() => states.money_list);
 
     return {
         money_list,
+
+        update_money,
+        delete_money,
         fetch_money_list,
     };
 });
