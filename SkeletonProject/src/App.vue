c3a5c9f04813e71ea0537a0ab04c202845ea7aa4
DYA <148474216+cheonghaa@users.noreply.github.com>
2024-06-11 16:55:27 +0900

    routing추가 프로필 페이지

--- SkeletonProject/src/App.vue
+++ SkeletonProject/src/App.vue
@@ -6,12 +6,5 @@
 </template>
 
 <script setup>
-// import { computed } from "vue";
 import Header from "./components/Header.vue";
-// import { useTodoListStore } from "@/stores/ItemList.js";
-
-// const todoListStore = useTodoListStore();
-
-// const fetchTodoList = todoListStore.fetchTodoList;
-// fetchTodoList();
 </script>
--- SkeletonProject/src/stores/ItemList.js
+++ SkeletonProject/src/stores/ItemList.js
@@ -4,35 +4,33 @@ import { reactive, computed } from "vue";
 import axios from "axios";
 
 // export
-export const use_money_list_store = defineStore ("money_list". () => {
+export const use_money_list_store = defineStore("money_list", () => {
     const BASEURI = "/api/items";
     const states = reactive({ money_list: [] });
 
-
     // money_list 목록 조회하기: fetch!!!
     const fetch_money_list = async () => {
-    try {
-        const response = await axios.get(BASEURI);
-        if (response.status === 200) {
-        state.money_list = response.data;
-        } else {
-        alert("데이터 조회 실패");
+        try {
+            const response = await axios.get(BASEURI);
+            if (response.status === 200) {
+                states.money_list = response.data;
+            } else {
+                alert("데이터 조회 실패");
+            }
+        } catch (error) {
+            alert("에러발생 :" + error);
         }
-    } catch (error) {
-        alert("에러발생 :" + error);
-    }
     };
-
 
     // money_list 아이템 추가
     // money_list 아이템 수정(변경)
     // money_list 삭제
 
     // money_list 반응성 주기
-    const money_list = computed(() => state.money_list)
+    const money_list = computed(() => states.money_list);
 
     return {
-    money_list,
-    fetch_money_list,
-    }
+        money_list,
+        fetch_money_list,
+    };
 });
