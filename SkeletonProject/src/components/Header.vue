1d88c24337d22e46b7b4da8186338f8df8efd475
DYA <148474216+cheonghaa@users.noreply.github.com>
2024-06-13 09:48:35 +0900

    최신화

--- SkeletonProject/src/components/Header.vue
+++ SkeletonProject/src/components/Header.vue
@@ -1,36 +1,31 @@
 <template>
-
-  <nav class="navbar navbar-expand-sm bg-warning navbar-dark sticky-top">
-    <div class="container justify-content-between">
-      <router-link to="/" class="navbar-brand" @click="fetch_money_list"
-        >가계부</router-link
-      >
-      <div class="collapse navbar-collapse" id="mynavbar1">
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item">
-            <PieChartLink />
-          </li>
-          <li class="nav-item">
-            <router-link to="/Chart" class="nav-link">통계</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/summary" class="nav-link">요약보기</router-link>
-          </li>
-        </ul>
-      </div>
-      <div class="d-flex justify-content-end">
-        <!-- 수정된 부분 -->
-        <div class="collapse navbar-collapse d-none d-sm-block" id="mynavbar">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <router-link to="/profile" class="nav-link">
-                <i class="fa-solid fa-user-tie"></i> 프로필
-              </router-link>
-            </li>
-          </ul>
-
+    <nav class="navbar navbar-expand-sm bg-warning navbar-dark sticky-top">
+        <div class="container justify-content-between">
+            <router-link to="/" class="navbar-brand" @click="fetch_money_list">가계부</router-link>
+            <div class="collapse navbar-collapse" id="mynavbar1">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <PieChartLink />
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/Chart" class="nav-link">통계</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/summary" class="nav-link">요약보기</router-link>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-flex justify-content-end">
+                <!-- 수정된 부분 -->
+                <div class="collapse navbar-collapse d-none d-sm-block" id="mynavbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link to="/profile" class="nav-link"> <i class="fa-solid fa-user-tie"></i> 프로필 </router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
     </nav>
 </template>
 
