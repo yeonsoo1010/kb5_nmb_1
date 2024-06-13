3acdaf3ecdbb4b6c89affa727ed68334b8b0efad
sooho <soolmagpie@naver.com>
2024-06-13 10:22:27 +0900

    [feat] chart 막대그래프 추가

--- SkeletonProject/src/pages/AddItem.vue
+++ SkeletonProject/src/pages/AddItem.vue
@@ -1,53 +1,84 @@
 <template>
-    <div>
-        <div class="row">
-            <div class="col p-3">
-                <h2>지출</h2>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col">
-                <div class="form-group mb-3">
-                    <label for="name">내역</label>
-                    <input type="text" class="form-control" id="moneyItem" v-model="moneyItem.name" />
-                </div>
-
-                <div class="form-group mb-3">
-                    <label for="price">가격</label>
-                    <input type="number" class="form-control" id="price" v-model="moneyItem.price" />
-                </div>
-
-                <div class="form-group mb-3">
-                    <label for="category">카테고리</label>
-                    <select class="form-control" v-model="moneyItem.category_id">
-                        <option value="4">식비</option>
-                        <option value="5">교통</option>
-                        <option value="6">쇼핑</option>
-                        <option value="7">의료</option>
-                        <option value="8">기타</option>
-                    </select>
-                </div>
-
-                <div class="form-group mb-3">
-                    <label for="memo">메모</label>
-                    <textarea class="form-control" id="memo" v-model="moneyItem.memo"></textarea>
-                </div>
-
-                <div class="form-group mb-3">
-                    <label for="date">날짜</label>
-                    <input type="date" class="form-control" id="date" v-model="moneyItem.date" />
-                </div>
-            </div>
-        </div>
-
-        <div class="row mt-4">
-            <div class="col text-center">
-                <button type="button" class="btn btn-primary m-1" @click="addMoneyHandler">추가</button>
-                <button type="button" class="btn btn-secondary m-1" @click="router.push('/')">취소</button>
-            </div>
-        </div>
+  <div>
+    <div class="row">
+      <div class="col p-3">
+        <h2>지출</h2>
+      </div>
     </div>
+
+    <div class="row">
+      <div class="col">
+        <div class="form-group mb-3">
+          <label for="name">내역</label>
+          <input
+            type="text"
+            class="form-control"
+            id="moneyItem"
+            v-model="moneyItem.name"
+          />
+        </div>
+
+        <div class="form-group mb-3">
+          <label for="price">가격</label>
+          <input
+            type="number"
+            class="form-control"
+            id="price"
+            v-model="moneyItem.price"
+          />
+        </div>
+
+        <div class="form-group mb-3">
+          <label for="category">카테고리</label>
+          <select class="form-control" v-model="moneyItem.category_id">
+            <option value="4">식비</option>
+            <option value="5">교통</option>
+            <option value="6">쇼핑</option>
+            <option value="7">의료</option>
+            <option value="8">기타</option>
+          </select>
+        </div>
+
+        <div class="form-group mb-3">
+          <label for="memo">메모</label>
+          <textarea
+            class="form-control"
+            id="memo"
+            v-model="moneyItem.memo"
+          ></textarea>
+        </div>
+
+        <div class="form-group mb-3">
+          <label for="date">날짜</label>
+          <input
+            type="date"
+            class="form-control"
+            id="date"
+            v-model="moneyItem.date"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-4">
+      <div class="col text-center">
+        <button
+          type="button"
+          class="btn btn-primary m-1"
+          @click="addMoneyHandler"
+        >
+          추가
+        </button>
+        <button
+          type="button"
+          class="btn btn-secondary m-1"
+          @click="router.push('/')"
+        >
+          취소
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -56,32 +87,38 @@ import { useRouter } from "vue-router";
 import { use_money_list_store } from "@/stores/ItemList.js";
 
 const router = useRouter();
-const { add_money, fetch_money_list } = use_money_list_store();
+const { fetch_money_list } = use_money_list_store();
+
 const moneyItem = reactive({
-    name: "",
-    price: 0,
-    category_id: "",
-    memo: "",
-    date: "",
+  name: "",
+  price: 0,
+  category_id: "",
+  memo: "",
+  date: "",
 });
 
-const addMoneyHandler = () => {
-    if (!moneyItem.name || moneyItem.name.trim() === "") {
-        alert("내역을 반드시 입력해야 합니다");
-        return;
-    }
+const addMoneyHandler = async () => {
+  if (!moneyItem.name || moneyItem.name.trim() === "") {
+    alert("내역을 반드시 입력해야 합니다");
+    return;
+  }
 
-    const newItem = {
-        name: moneyItem.name,
-        price: parseFloat(moneyItem.price),
-        category_id: moneyItem.category_id,
-        datetime: moneyItem.date ? new Date(moneyItem.date).toISOString() : new Date().toISOString(),
-        memo: moneyItem.memo,
-    };
+  const newItem = {
+    name: moneyItem.name,
+    price: parseFloat(moneyItem.price),
+    category_id: moneyItem.category_id,
+    datetime: moneyItem.date
+      ? new Date(moneyItem.date).toISOString()
+      : new Date().toISOString(),
+    memo: moneyItem.memo,
+  };
 
-    add_money(newItem, () => {
-        fetch_money_list(); // 목록 갱신
-        router.push("/");
-    });
+  try {
+    await axios.post("/api/items", newItem);
+    fetch_money_list(); // 목록 갱신
+    router.push("/");
+  } catch (error) {
+    alert("에러 발생: " + error);
+  }
 };
 </script>
--- SkeletonProject/src/pages/Chart.vue
+++ SkeletonProject/src/pages/Chart.vue
@@ -1,38 +1,81 @@
 <template>
   <div class="chart-container">
-    <div class="chart">
-      <canvas ref="chartCanvas" style="width: 500px; height: 300px"></canvas>
+    <div class="chart-wrapper">
+      <div class="controls">
+        <div class="custom-select">
+          <button @click="toggleDropdown" class="selected-option">
+            {{ selectedMonth }}
+          </button>
+          <ul class="dropdown" :class="{ show: isDropdownOpen }">
+            <li
+              v-for="month in availableMonths"
+              :key="month"
+              @click="selectMonth(month)"
+            >
+              {{ month }}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="chart-area">
+        <canvas ref="chartCanvas" style="width: 400px; height: 400px"></canvas>
+      </div>
     </div>
-    <div class="legend">
-      <h3>총 지출액: {{ totalAmount.toLocaleString() }}원</h3>
-      <br />
-      <h4>세부 항목</h4>
-      <ul>
-        <li v-for="(label, index) in chartLabels" :key="index">
-          <span
-            class="legend-color"
-            :style="{ backgroundColor: colors[index] }"
-          ></span>
-          {{ label }}: {{ chartAmounts[index].toLocaleString() }}원
-        </li>
-      </ul>
+    <div class="info">
+      <div class="total-amount">
+        <h3>총 지출액</h3>
+        <p>{{ totalAmount.toLocaleString() }}원</p>
+      </div>
+      <div class="legend">
+        <h3>세부 항목</h3>
+        <ul>
+          <li v-for="(label, index) in chartLabels" :key="index">
+            <span
+              class="legend-color"
+              :style="{ backgroundColor: colors[index % colors.length] }"
+            ></span>
+            {{ label }}: {{ chartAmounts[index].toLocaleString() }}원
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="total-amount-chart">
+      <canvas
+        id="totalAmountChart"
+        style="width: 600px; height: 400px"
+      ></canvas>
     </div>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import axios from 'axios';
 import Chart from 'chart.js/auto';
 
 const chartAmounts = ref([]);
 const chartLabels = ref([]);
 const colors = ['#FFC94A', '#C08B5C', '#795458', 'rgb(87, 96, 101)', '#81A263'];
-
 const chartCanvas = ref(null);
-let totalAmount = 0; // 총 지출액을 저장할 변수
+const selectedMonth = ref('');
+const availableMonths = ref([]);
+const isDropdownOpen = ref(false);
+const totalAmount = ref(0); // 총 지출액을 ref로 선언
+let chartInstance = null; // Chart 인스턴스를 저장할 변수
+let totalAmountChartInstance = null; // 총 지출액 막대그래프 인스턴스를 저장할 변수
+let outcomeCategories = [];
+let expenses = {};
 
-onMounted(async () => {
+const toggleDropdown = () => {
+  isDropdownOpen.value = !isDropdownOpen.value;
+};
+
+const selectMonth = (month) => {
+  selectedMonth.value = month;
+  isDropdownOpen.value = false;
+};
+
+const fetchData = async () => {
   try {
     const itemsResponse = await axios.get('http://localhost:3000/items');
     const categoriesResponse = await axios.get(
@@ -42,78 +85,178 @@ onMounted(async () => {
     const items = itemsResponse.data;
     const categories = categoriesResponse.data;
 
-    const outcomeCategories = categories.filter(
+    outcomeCategories = categories.filter(
       (category) => category.type === 'outcome'
     );
+
     const outcomeCategoryMap = outcomeCategories.reduce((acc, category) => {
       acc[category.id] = { title: category.title, total: 0 };
       return acc;
     }, {});
 
-    totalAmount = 0; // 총 지출액 초기화
+    expenses = {};
 
     items.forEach((item) => {
       if (outcomeCategoryMap[item.category_id]) {
-        outcomeCategoryMap[item.category_id].total += parseFloat(item.price);
-        totalAmount += parseFloat(item.price); // 총 지출액 누적
+        const date = new Date(item.datetime);
+        const month = `${date.getFullYear()}-${(date.getMonth() + 1)
+          .toString()
+          .padStart(2, '0')}`;
+
+        if (!expenses[month]) {
+          expenses[month] = { totalAmount: 0, categoryAmounts: {} };
+        }
+
+        if (!expenses[month].categoryAmounts[item.category_id]) {
+          expenses[month].categoryAmounts[item.category_id] = 0;
+        }
+
+        expenses[month].categoryAmounts[item.category_id] += parseFloat(
+          item.price
+        );
+        expenses[month].totalAmount += parseFloat(item.price);
       }
     });
 
-    chartLabels.value = outcomeCategories.map((category) => category.title);
-    chartAmounts.value = outcomeCategories.map(
-      (category) => outcomeCategoryMap[category.id].total
-    );
+    availableMonths.value = Object.keys(expenses).sort();
+    if (availableMonths.value.length > 0) {
+      selectedMonth.value = availableMonths.value[0];
+    }
 
-    const ctx = chartCanvas.value.getContext('2d');
-    new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-        labels: chartLabels.value,
-        datasets: [
-          {
-            data: chartAmounts.value.map((amount) =>
-              ((amount / totalAmount) * 100).toFixed(1)
-            ),
-            backgroundColor: colors,
-            borderColor: '#fff',
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        aspectRatio: 1.3,
-        layout: {
-          padding: {
-            top: 20,
-            bottom: 20,
-            left: 20,
-            right: 20,
-          },
-        },
-        plugins: {
-          legend: {
-            display: false,
-          },
-          tooltip: {
-            callbacks: {
-              label: (context) => {
-                let label = context.label || '';
-                if (label) {
-                  label += ': ';
-                }
-                const value = context.parsed || 0;
-                label += value.toFixed(1) + '%';
-                return label;
-              },
-            },
-          },
-        },
-      },
+    // Update chartLabels with category titles using category IDs
+    chartLabels.value = Object.keys(
+      expenses[selectedMonth.value].categoryAmounts
+    ).map((id) => {
+      const category = outcomeCategories.find((category) => category.id === id);
+      return category ? category.title : '기타';
     });
+
+    return { outcomeCategories, expenses };
   } catch (error) {
     console.error('Error fetching data:', error);
   }
+};
+
+const updateChart = () => {
+  const monthData = expenses[selectedMonth.value];
+  if (!monthData) return;
+
+  totalAmount.value = monthData.totalAmount;
+  chartLabels.value = Object.keys(monthData.categoryAmounts).map((id) => {
+    const category = outcomeCategories.find((category) => category.id === id);
+    return category ? category.title : '기타';
+  });
+  chartAmounts.value = Object.values(monthData.categoryAmounts);
+
+  const ctx = chartCanvas.value.getContext('2d');
+
+  if (chartInstance) {
+    chartInstance.destroy();
+  }
+
+  chartInstance = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: chartLabels.value,
+      datasets: [
+        {
+          data: chartAmounts.value,
+          backgroundColor: colors,
+          borderColor: '#fff',
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      aspectRatio: 0.8, // 가로 및 세로 비율 조정
+      layout: {
+        padding: {
+          top: 20,
+          bottom: 20,
+          left: 20,
+          right: 20,
+        },
+      },
+      plugins: {
+        legend: {
+          display: true,
+        },
+        tooltip: {
+          callbacks: {
+            label: (context) => {
+              let label = context.label || '';
+              if (label) {
+                label += ': ';
+              }
+              const value = context.raw || 0;
+              label += value.toLocaleString() + '원';
+              return label;
+            },
+          },
+        },
+        doughnutLabel: {
+          labels: [
+            {
+              render: 'percentage',
+              fontColor: '#fff',
+              precision: 0,
+              fontSize: 14,
+            },
+          ],
+        },
+      },
+    },
+  });
+
+  // 월별 총 지출액 차트 생성
+  if (totalAmountChartInstance) {
+    totalAmountChartInstance.destroy();
+  }
+
+  const totalAmountCtx = document
+    .getElementById('totalAmountChart')
+    .getContext('2d');
+  totalAmountChartInstance = new Chart(totalAmountCtx, {
+    type: 'bar',
+    data: {
+      labels: availableMonths.value,
+      datasets: [
+        {
+          label: '월별 총 지출액',
+          data: availableMonths.value.map(
+            (month) => expenses[month]?.totalAmount || 0
+          ),
+          backgroundColor: '#007bff', // 색상은 여기에 맞게 수정하세요.
+          borderColor: '#007bff',
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      indexAxis: 'x', // 가로 막대그래프로 설정
+      responsive: true,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        title: {
+          // 새로운 title 객체 추가
+          display: true,
+          text: '월별 총 지출액',
+          fontSize: 30, // 월별 총 지출액 제목 글자 크기 조절
+        },
+      },
+      aspectRatio: 1.2,
+    },
+  });
+};
+
+onMounted(async () => {
+  await fetchData();
+  updateChart();
 });
+
+watch(selectedMonth, updateChart);
 </script>
 
 <style scoped>
@@ -123,8 +266,28 @@ onMounted(async () => {
   align-items: center;
 }
 
-.chart {
-  flex: 1;
+.chart-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.chart-area {
+  margin-right: 50px; /* 도넛 차트와 정보 사이 간격 조절 */
+}
+
+.info {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end; /* 총 지출액 및 세부 항목 오른쪽 정렬 */
+}
+
+.total-amount-chart {
+  margin-left: 50px; /* 막대 그래프와 정보 사이 간격 조절 */
+}
+
+.total-amount {
+  margin-bottom: 20px;
 }
 
 .legend {
@@ -152,5 +315,49 @@ onMounted(async () => {
   margin-right: 5px;
   border-radius: 50%;
   display: inline-block;
+}
+
+.custom-select {
+  position: relative;
+  display: inline-block;
+}
+
+.selected-option {
+  background-color: #f9f9f9;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  cursor: pointer;
+  width: 150px; /* 선택 상자의 너비 조정 */
+}
+
+.dropdown {
+  position: absolute;
+  top: calc(100% + 5px); /* 선택 상자 아래에 나타나도록 위치 조정 */
+  left: 0;
+  z-index: 10;
+  width: 150px; /* 드롭다운 메뉴의 너비 조정 */
+  max-height: 200px;
+  overflow-y: auto;
+  border: 1px solid #ccc;
+  border-top: none;
+  border-radius: 0 0 5px 5px;
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: max-height 0.3s ease-in-out;
+  display: none;
+}
+
+.dropdown.show {
+  display: block;
+}
+
+.dropdown li {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.dropdown li:hover {
+  background-color: #f0f0f0;
 }
 </style>
