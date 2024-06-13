9b37e2f0976506ce576a0721ea50efe56e496557
yeonsoo1010 <meriel1010@hanyang.ac.kr>
2024-06-13 16:09:26 +0900

    [feat] 홈파일과 구별되는 trashcan 삭제변경 파일을 생성 홈, 요약 페이지에서 자동으로 뜨도록

--- SkeletonProject/src/pages/Home.vue
+++ SkeletonProject/src/pages/Home.vue
@@ -1,18 +1,23 @@
 <template>
     <div>
         <div class="row align-items-center justify-content-between">
-            <div class="col-auto p-3">
+            <!-- 가계부를 클릭하면 fetch -->
+            <!-- <div class="col-auto p-3">
                 <button class="btn btn-primary" @click="fetch_money_list">목록 보기</button>
-            </div>
+            </div> -->
             <div class="col-auto p-3">
-                <button class="btn btn-outline-secondary"><i class="fa-regular fa-trash-can"></i></button>
+                <button class="btn btn-outline-secondary">
+                    <router-link to="/home/edit/editlist">
+                        <i class="fa-regular fa-trash-can"> </i>
+                    </router-link>
+                </button>
             </div>
         </div>
 
         <div class="row justify-content-center">
             <div class="col-7">
                 <ul class="list-group">
-                    <HomeItem v-for="moneyItem in money_list" :key="moneyItem.id" :moneyItem="moneyItem" />
+                    <HomeItem_edit_delete v-for="moneyItem in money_list" :key="moneyItem.id" :moneyItem="moneyItem" />
                 </ul>
             </div>
         </div>
@@ -25,12 +30,14 @@
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
 import { use_money_list_store } from "@/stores/ItemList.js";
-import HomeItem from "@/pages/HomeItem_edit_delete.vue";
+import HomeItem_edit_delete from "@/pages/HomeItem_edit_delete.vue";
 import Footer from "@/components/Footer.vue";
 
 const money_list_store = use_money_list_store();
 const { fetch_money_list } = money_list_store;
 const money_list = computed(() => money_list_store.money_list);
+
+onMounted(fetch_money_list);
 </script>
