ecb314de4afbc0cec93f851524b0767e6203f20e
sooho <soolmagpie@naver.com>
2024-06-14 09:42:21 +0900

    [수정]최종

--- SkeletonProject/src/pages/Home.vue
+++ SkeletonProject/src/pages/Home.vue
@@ -1,34 +1,31 @@
 <template>
-    <div>
-        <div class="row align-items-center justify-content-between">
-            <div class="col-auto p-3">
-                <router-link to="/home/edit/editlist">
-                    <button class="btn btn-outline-secondary">
-                        <i class="fa-regular fa-trash-can"> </i>
-                    </button>
-                </router-link>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-7">
-                <div class="list-container">
-                    <ul class="list-group">
-                        <HomeItem />
-                        <!-- v-for="moneyItem in sortedMoneyList"
-              :key="moneyItem.id"
-              :moneyItem="moneyItem" -->
-                        <Footer></Footer>
-                    </ul>
-                </div>
-            </div>
-        </div>
+  <div>
+    <div class="row align-items-center justify-content-between">
+      <div class="col-auto p-3">
+        <router-link to="/home/edit/editlist">
+          <button class="btn btn-outline-secondary">
+            <i class="fa-regular fa-trash-can"> </i>
+          </button>
+        </router-link>
+      </div>
     </div>
+    <div class="row justify-content-center">
+      <div class="col-7">
+        <div class="list-container">
+          <ul class="list-group">
+            <HomeItem />
+            <Footer></Footer>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
-import { computed, onMounted } from "vue";
-import { use_money_list_store } from "@/stores/ItemList.js";
-import Footer from "@/components/Footer.vue";
-import HomeItem from "@/pages/HomeItem.vue";
+import { computed, onMounted } from 'vue';
+import { use_money_list_store } from '@/stores/ItemList.js';
+import Footer from '@/components/Footer.vue';
+import HomeItem from '@/pages/HomeItem.vue';
 
 const money_list_store = use_money_list_store();
 const { fetch_money_list } = money_list_store;
@@ -37,15 +34,15 @@ const money_list = computed(() => money_list_store.money_list);
 onMounted(fetch_money_list);
 
 const sortedMoneyList = computed(() => {
-    return money_list.value.slice().sort((a, b) => {
-        return new Date(a.date) - new Date(b.date);
-    });
+  return money_list.value.slice().sort((a, b) => {
+    return new Date(a.date) - new Date(b.date);
+  });
 });
 </script>
 
 <style scoped>
 .list-container {
-    max-height: 550px;
-    overflow-y: auto; /* 세로 스크롤 활성화 */
+  max-height: 550px;
+  overflow-y: auto; /* 세로 스크롤 활성화 */
 }
 </style>
